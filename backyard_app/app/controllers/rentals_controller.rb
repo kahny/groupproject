2eaf41d86e4d02d7b840158ac2c7b3c83d03340c
DoriPS13 <palmer.sanderson>
2014-08-31 12:43:47 -0700

    setting up controllers

---
+++ backyard_app/app/controllers/rentals_controller.rb
@@ -0,0 +1,4 @@
+class RentalsController < ApplicationController
+  def index
+  end
+end
--- backyard_app/config/routes.rb
+++ backyard_app/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'reviews/index'
+
+  get 'rentals/index'
+
+  get 'users/index'
+
+  get 'user/index'
+
   resources :users do
       resources :reviews
       resources :rentals
