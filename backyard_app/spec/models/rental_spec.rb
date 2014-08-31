ed47e50f19ff46f2e832e612fe5dce9f1e63a6e9
Connie Chang <changchilla@gmail.com>
2014-08-30 22:08:09 -0700

    added testing for user, review, and rental creation

--- backyard_app/spec/models/rental_spec.rb
+++ backyard_app/spec/models/rental_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Rental, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "should test that a rental is saved" do
+		bobs_rental = Rental.create(title: "Bob's luxury home", description: "Close to everything", location: "711 711th Street San Francisco", price: 240, category: "pool", image_url: "testurl.com")
+		expect(bobs_rental).not_to eq(nil)
+	end
 end
--- backyard_app/spec/models/review_user_spec.rb
+++ backyard_app/spec/models/review_user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ReviewUser, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "should test that a review is saved in relation to a user" do
+		#create a user
+		bob =  User.create(first_name: "Bob", last_name: "Sanderson", email: "bobby@bob.com", password_digest: "12341234", isOwner: true, location: "location")
+		#create a valid review
+		bobs_review = Review.create(rating: 4, description: "So close to so good but not bad")
+		#assign bobs review to bob
+		bob.reviews << bobs_review
+		#check for relationship between bob and his review
+		expect(bob.reviews).not_to eq(nil)
+		expect(bobs_review.users).not_to eq(nil)
+	end
+
+
 end
