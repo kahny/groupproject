35053c1af3bdc09db6283f9ea8645ab10bb1dabb
DoriPS13 <palmer.sanderson>
2014-08-31 13:34:40 -0700

    basic users controller and bootstrap

--- backyard_app/app/controllers/users_controller.rb
+++ backyard_app/app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
 
   def index
+  	@users = User.all
+  end
+
+  def new
+  	@user = User.new
 
   end
 
+  def create
+
+  	new_user = params.require(:user).permit(
+  		:first_name,
+  		:last_name,
+  		:email,
+  		:password,
+  		:isOwner,
+  		:location)
+  	@user = User.create(new_user)
+
+  	redirect_to user
+  end
 end
