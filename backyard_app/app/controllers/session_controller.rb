3049b1a0039e02416079c42118a92019b07a6a91
msalli <alli.cernoch@gmail.com>
2014-08-31 22:07:04 -0700

    adding login form at views/session/new

--- backyard_app/app/controllers/session_controller.rb
+++ backyard_app/app/controllers/session_controller.rb
@@ -1,8 +1,19 @@
 class SessionController < ApplicationController
   def new
+    #login form
   end
 
   def create
+    #where auth happens
+    @user = User.authenticate(params[:user][:email], params[:user][:password])
+
+    if @user
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash.now[:notice] = "There was a problem with your username or password"
+      render :new
+    end
   end
 
   def destroy
