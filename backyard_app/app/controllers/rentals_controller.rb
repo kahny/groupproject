b987172b8ea91b48cc965ce0dd66a7ea538f3770
msalli <alli.cernoch@gmail.com>
2014-09-01 00:14:41 -0700

    added before_action and current user to rentals application

--- backyard_app/app/controllers/rentals_controller.rb
+++ backyard_app/app/controllers/rentals_controller.rb
@@ -1,32 +1,38 @@
 class RentalsController < ApplicationController
 
+  before_action :is_authenticated?
+
   def index
   end
 
 
   def new
+    @user = current_user
+    @rental = @user.rentals.new
 
-  	user_id = params[:user_id]
-  	@user = User.find_by_id(user_id)
-  	@rental = @user.rentals.new
+    if User.find(params[:user_id]) != @current_user
+      redirect_to root_path
+    end
   end
 
   def create
-  	@user = params[:id] #change to current user when sessions are done
+  	if User.find(params[:user_id]) == current_user
+    	new_rental = params.require(:rental).permit(
+    		:title,
+    		:description,
+    		:address,
+    		:price,
+    		:catagory,
+    		:image_url)
+    	rental = Rental.create(new_rental)
 
-  	new_rental = params.require(:rental).permit(
-  		:title,
-  		:description,
-  		:address,
-  		:price,
-  		:catagory,
-  		:image_url)
-  	rental = Rental.create(new_rental)
-
-  	@user.rentals << rental
-
-
-  	redirect_to user_rentals_path(new_rental)
+      @user = current_user
+      @user.rentals << rental
+      redirect_to user_rentals_path(new_rental)
+    else
+      flash[:notice] = "Please login"
+      redirect_to login_path
+    end
   end
 
 
