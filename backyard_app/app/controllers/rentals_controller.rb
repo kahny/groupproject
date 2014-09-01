21ba14f1f5dfb937151bc888089cd9ccf593f5cd
DoriPS13 <palmer.sanderson>
2014-08-31 18:50:05 -0700

    added geocoder working on rentals form

--- backyard_app/app/controllers/rentals_controller.rb
+++ backyard_app/app/controllers/rentals_controller.rb
@@ -1,4 +1,31 @@
 class RentalsController < ApplicationController
+
   def index
   end
+
+
+  def new
+  	@user = params[:id]
+  	@rental = @user.rental.new
+  end
+
+  def create
+  	@user = params[:id] #change to current user
+
+  	new_rental = params.require(:rental).permit(
+  		:title,
+  		:description,
+  		:address,
+  		:price,
+  		:catagory,
+  		:image_url)
+  	rental = Rental.create(new_rental)
+
+  	@user.rentals << rental
+
+
+  	redirect_to user_rentals_path(new_rental)
+  end
+
+
 end
