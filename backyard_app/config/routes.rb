c63f400b59355be51fa761b3fb9007e365f17a1b
msalli <alli.cernoch@gmail.com>
2014-08-31 21:35:13 -0700

    fixed spelling error in development.rb, adding methods to site controller, routes for about, contact

--- backyard_app/config/routes.rb
+++ backyard_app/config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   get "/rentals", to: "rentals#index", as: "browse"
 
+  get "/about", to: "site#about", as: "about"
+
+  get "/contact", to: "site#contact", as: "contact"
+
   resources :users do
       resources :reviews
       resources :rentals
