aa5d8638ef6634d974067ea748152ff020f53014
DoriPS13 <palmer.sanderson>
2014-08-31 23:39:09 -0700

    able to create new rentals

--- backyard_app/app/controllers/rentals_controller.rb
+++ backyard_app/app/controllers/rentals_controller.rb
@@ -12,21 +12,21 @@ class RentalsController < ApplicationController
   end
 
   def create
-  	@user = params[:id] #change to current user when sessions are done
-
+  	user_id = params[:user_id] #change to current user when sessions are done
+  	@user = User.find_by_id(user_id)
   	new_rental = params.require(:rental).permit(
   		:title,
   		:description,
-  		:address,
+  		:location,
   		:price,
-  		:catagory,
+  		:category,
   		:image_url)
-  	rental = Rental.create(new_rental)
+  	my_rental = Rental.create(new_rental)
 
-  	@user.rentals << rental
+  	@user.rentals << my_rental
 
 
-  	redirect_to user_rentals_path(new_rental)
+  	redirect_to user_rentals_path(my_rental)
   end
 
 
