d46b85561d92c9b432cf0088b3d8d25111f42a2f
msalli <alli.cernoch@gmail.com>
2014-09-02 16:50:42 -0700

    hide review form field using jquery, show onclick

---
+++ backyard_app/app/assets/javascripts/rentals.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $("#rental-review").hide();
+
+  $("#review-button").click(function() {
+    $("#rental-review").show();
+  });
+
+
+
+  // onclick for "Leave a comment" make form appear
+
+  // ajax: append review body to page.
+
+}); //closing all
--- backyard_app/app/controllers/reviews_controller.rb
+++ backyard_app/app/controllers/reviews_controller.rb
@@ -4,18 +4,18 @@ class ReviewsController < ApplicationController
 
   def create
     #so we need to figure out how to get the length of reviews AKA Total reviews of either location or owner
-
     #also how to set up the relations between ratings
-    # @current_user = current_user
+
     @user = User.find_by_id(params[:user_id])
     @rental = Rental.find_by_id(params[:rental_id])
     review_params = params[:review].permit(:user_id, :rating, :description)
     @review = @rental.reviews.create(review_params)
-    redirect_to "/users/#{@rental.user.id}/rentals/#{@rental.id}"
+
+    #Alli: commented this out, now using AJAX
+    # redirect_to "/users/#{@rental.user.id}/rentals/#{@rental.id}"
   end
 
   def show
-
   end
 
 end
