a909df321addf52e09bb03d917ac119d2f682533
DoriPS13 <palmer.sanderson>
2014-09-01 22:51:37 -0700

    leaving comments for tomorrow

--- backyard_app/app/controllers/reviews_controller.rb
+++ backyard_app/app/controllers/reviews_controller.rb
@@ -6,15 +6,15 @@ class ReviewsController < ApplicationController
   	new_review = params.require(:review).permit(
   		:rating,
   		:description)
+
+# wondering is this is the correct id or if it should be current user
   	reviewer = User.find_by_id(params[:user_id])
   	rental = Rental.find_by_id(params[:id])
 
-  	#so we need to figure out how to get the length of reviews
+#so we need to figure out how to get the length of reviews AKA Total reviews of either location or owner
 
+#also how to set up the relations between ratings
 
 
   end
-
-
-
 end
