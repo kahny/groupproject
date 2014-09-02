d54c5e2d2ae46847b5ecb388eddf9bc780594b1b
msalli <alli.cernoch@gmail.com>
2014-09-01 17:03:04 -0700

    removed problem causing links from nav

--- backyard_app/app/controllers/site_controller.rb
+++ backyard_app/app/controllers/site_controller.rb
@@ -1,8 +1,8 @@
 class SiteController < ApplicationController
 
   def index
-    id = current_user.id
-    @user = User.find(id)
+    id = params[:user_id]
+    @user = User.find_by_id(id)
   end
 
   def about
