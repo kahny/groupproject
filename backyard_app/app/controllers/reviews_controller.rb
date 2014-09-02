c3f6309d36cc0ca45aaf39a99634e85e6b1e4f1f
DoriPS13 <palmer.sanderson>
2014-09-01 20:47:43 -0700

    small commit

--- backyard_app/app/controllers/reviews_controller.rb
+++ backyard_app/app/controllers/reviews_controller.rb
@@ -6,6 +6,18 @@ class ReviewsController < ApplicationController
   	new_review = params.require(:review).permit(
   		:rating,
   		:description)
+  	reviewer = User.find_by_id(params[:user_id])
+  	rental = Rental.find_by_id(params[#TBD])
+
+
+
+
+
+
+
+
   end
+
+
 
 end
