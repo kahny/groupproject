7ceead63fdfa04143a51afef0e19995b3f021754
David Javate <davidjavate@David-Javates-MacBook-Pro-2.local>
2014-09-03 15:51:46 -0700

    fixed user reviews

---
+++ backyard_app/app/assets/javascripts/users.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+  $("#user-review").hide();
+
+  $("#userreview-button").click(function() {
+    $("#user-review").toggle();
+  });
+
+  $("#user-review").on('submit', function(e) {
+    e.preventDefault();
+
+    var rating = $("#rating-val").val();
+    var description = $("#description-val").val();
+    var user = $("#user-val").val();
+    var formURL = $(this).attr('action');
+
+    function reviewsToPage() {
+      $.ajax({
+      type: 'POST',
+      url: formURL,
+      data: {review: {user: user, rating: rating, description: description}},
+      success: function(data) {
+        console.log("Success with data!", data);
+
+        // append new review to page
+        $("#userreview-display").append(
+          data.description);
+
+        $("#userreview-display").append(
+          data.rating);
+
+        // reset form values
+        $("#rating-val").val(1);
+        $("#description-val").val("");
+      },
+      error: function(data) {
+        console.log("ERROR");
+      }
+    }); //closing AJAX
+    } //closing function reviewsToPage
+
+    reviewsToPage();
+  }); //closing on submit
+
+
+
+}); //closing all
--- backyard_app/spec/controllers/reviews_controller_spec.rb
+++ backyard_app/spec/controllers/reviews_controller_spec.rb
@@ -9,4 +9,6 @@ RSpec.describe ReviewsController, :type => :controller do
     end
   end
 
+
+
 end
